b68d912a4a9e5a823c7147a423a0b5079ba7e254
KristianCh <chovancak.kristian@gmail.com>
2021-02-17 19:13:09 +0100

    debugging

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ server.listen({ port: process.env.PORT, host: process.env.HOST}, () => {
 
 app.listen(process.env.port)
 
-var io = require('socket.io');
+var io = require('socket.io').listen(app);
 
 game_server = require('./gameServer')
 
